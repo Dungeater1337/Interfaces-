86733d5551a9de57ad578bf22398060ed1428802
judenfors <111962656+Dungeater1337@users.noreply.github.com>
2023-09-28 11:14:28 +0200

    s
    s

---
+++ Cylinder.cs
@@ -0,0 +1,7 @@
+namespace Interfaces_
+{
+    public class Cylinder
+    {
+        
+    }
+}
---
+++ IShape .cs
@@ -0,0 +1,12 @@
+
+interface IShape 
+{
+   void Area()
+   {
+     
+   }
+   void Circumference()
+   {
+
+   }
+}
